51bdede9f9753b3e2d0ad32b193111e0dc80975c
eoin <g00431577@atu.ie>
2025-04-27 03:12:02 +0100

    constructor success test created

--- src/main/java/ie/atu/week8/Passenger.java
+++ src/main/java/ie/atu/week8/Passenger.java
@@ -12,5 +12,27 @@ public class Passenger {
         this.LastName = LastName;
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
 }

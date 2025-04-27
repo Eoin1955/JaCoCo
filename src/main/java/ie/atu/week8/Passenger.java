3c24a2339ed8511fe57aa71dab21c2470dca2a61
eoin <g00431577@atu.ie>
2025-04-27 03:09:45 +0100

    updated pom file and created test class and passenger class

---
+++ src/main/java/ie/atu/week8/Passenger.java
@@ -0,0 +1,16 @@
+package ie.atu.week8;
+
+public class Passenger {
+
+    private String title;
+    private String FirstName;
+    private String LastName;
+
+    public Passenger(String title, String FirstName, String LastName) {
+        this.title = title;
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+    }
+
+
+}
